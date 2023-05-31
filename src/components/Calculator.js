e50bf3b18e5eedc0f807d0c2f416dafd738fa7fa
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-31 13:27:38 -0300

    Add Calulator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Calculator = () => (
+  <section className="calculator">
+    <DisplayCalculator />
+  </section>
+);
+
+const DisplayCalculator = () => (
+  <div className="btns-container">
+    <div className="screen">0</div>
+    <button type="button" className="keys top-left">AC</button>
+    <button type="button" className="keys">+/-</button>
+    <button type="button" className="keys">%</button>
+    <button type="button" className="keys orange">÷</button>
+    <button type="button" className="keys">7</button>
+    <button type="button" className="keys">8</button>
+    <button type="button" className="keys">9</button>
+    <button type="button" className="keys orange">X</button>
+    <button type="button" className="keys">4</button>
+    <button type="button" className="keys">5</button>
+    <button type="button" className="keys">6</button>
+    <button type="button" className="keys orange">-</button>
+    <button type="button" className="keys">1</button>
+    <button type="button" className="keys">2</button>
+    <button type="button" className="keys">3</button>
+    <button type="button" className="keys orange">+</button>
+    <button type="button" className="keys span-2">0</button>
+    <button type="button" className="keys">.</button>
+    <button type="button" className="keys orange">=</button>
+  </div>
+);
+export default Calculator;
