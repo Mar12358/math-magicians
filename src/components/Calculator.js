afe9682786ed5fbed65866810f58bdb0dec881d3
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-01 01:10:32 -0300

    Modify class names

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,25 +10,25 @@ const Calculator = () => (
 const DisplayCalculator = () => (
   <div className="btns-container">
     <div className="screen">0</div>
-    <button type="button" className="keys top-left">AC</button>
-    <button type="button" className="keys">+/-</button>
-    <button type="button" className="keys">%</button>
-    <button type="button" className="keys orange">÷</button>
-    <button type="button" className="keys">7</button>
-    <button type="button" className="keys">8</button>
-    <button type="button" className="keys">9</button>
-    <button type="button" className="keys orange">X</button>
-    <button type="button" className="keys">4</button>
-    <button type="button" className="keys">5</button>
-    <button type="button" className="keys">6</button>
-    <button type="button" className="keys orange">-</button>
-    <button type="button" className="keys">1</button>
-    <button type="button" className="keys">2</button>
-    <button type="button" className="keys">3</button>
-    <button type="button" className="keys orange">+</button>
-    <button type="button" className="keys zero-btn">0</button>
-    <button type="button" className="keys">.</button>
-    <button type="button" className="keys orange">=</button>
+    <button type="button" className="btn">AC</button>
+    <button type="button" className="btn">+/-</button>
+    <button type="button" className="btn">%</button>
+    <button type="button" className="btn orange">÷</button>
+    <button type="button" className="btn">7</button>
+    <button type="button" className="btn">8</button>
+    <button type="button" className="btn">9</button>
+    <button type="button" className="btn orange">X</button>
+    <button type="button" className="btn">4</button>
+    <button type="button" className="btn">5</button>
+    <button type="button" className="btn">6</button>
+    <button type="button" className="btn orange">-</button>
+    <button type="button" className="btn">1</button>
+    <button type="button" className="btn">2</button>
+    <button type="button" className="btn">3</button>
+    <button type="button" className="btn orange">+</button>
+    <button type="button" className="btn zero-btn">0</button>
+    <button type="button" className="btn">.</button>
+    <button type="button" className="btn orange">=</button>
   </div>
 );
 export default Calculator;
